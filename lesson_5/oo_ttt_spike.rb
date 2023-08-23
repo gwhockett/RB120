b9394ba8018b33797d2de53dca1e9a0370ec8d66
George Hockett <gwhockett@gmail.com>
2023-08-23 12:53:36 -0400

    rubocopped oo tic  tac toe

--- lesson_5/oo_ttt_spike.rb
+++ lesson_5/oo_ttt_spike.rb
@@ -1,12 +1,10 @@
-require 'pry'
-
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +   # columns
-                [[1, 5, 9], [3, 5, 7]]                # diagonals
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
   def initialize
     @squares = {}
-    (1..9).each {|key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   def []=(key, marker)
@@ -14,7 +12,7 @@ class Board
   end
 
   def no_move_keys
-    @squares.keys.select{|key| @squares[key].unmarked? }
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 
   def full?
@@ -25,25 +23,25 @@ class Board
     !!winning_marker
   end
 
-  def all_human_line?(line)
-    @squares.values_at(*line).map(&:marker).count(TTTGame::HUMAN_MARK) == 3
+  def three_mark(line)
+    @squares.values_at(*line).map(&:marker)
   end
 
-  def all_computer_line?(line)
-    @squares.values_at(*line).map(&:marker).count(TTTGame::COMPUTER_MARK) == 3
+  def winning_line?(line)
+    three_mark(line).uniq.size == 1 && !@squares[line[0]].unmarked?
   end
-  #returns winning marker or nil
+
+  # returns winning marker or nil
+
   def winning_marker
     WINNING_LINES.each do |line|
-      if all_human_line?(line)
-        return TTTGame::HUMAN_MARK
-      elsif all_computer_line?(line)
-        return TTTGame::COMPUTER_MARK
-      end
+      return @squares[line[0]].marker if winning_line?(line)
     end
     nil
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def draw
     puts "     |     |"
     puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
@@ -57,9 +55,11 @@ class Board
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
     puts "     |     |"
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 
   def reset_squares
-    (1..9).each {|key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 end
 
@@ -90,14 +90,18 @@ end
 
 class TTTGame
   attr_reader :board, :human, :computer
+
   HUMAN_MARK = "X"
   COMPUTER_MARK = "O"
-
+  FIRST_TO_MOVE = HUMAN_MARK
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARK)
     @computer = Player.new(COMPUTER_MARK)
+    @current_marker = FIRST_TO_MOVE
   end
+
+  private
 
   def clear
     system 'clear'
@@ -126,6 +130,7 @@ class TTTGame
 
   def display_result
     clear_screen_and_display_board
+
     case board.winning_marker
     when human.marker
       puts "You won!"
@@ -139,7 +144,7 @@ class TTTGame
   def human_moves
     square = nil
     puts "Please select an open square from:"
-    puts "#{board.no_move_keys.join(', ')}"
+    puts board.no_move_keys.join(', ')
     loop do
       square = gets.chomp.to_i
       break if board.no_move_keys.include?(square)
@@ -152,7 +157,19 @@ class TTTGame
   def computer_moves
     board[board.no_move_keys.sample] = computer.marker
   end
-  
+
+  def human_turn?
+    return true if board.no_move_keys.size.odd?
+
+    false
+  end
+
+  def current_player_moves
+    return human_moves if human_turn?
+
+    computer_moves
+  end
+
   def play_again?
     answer = nil
     loop do
@@ -173,23 +190,31 @@ class TTTGame
     clear
   end
 
-  def play
-    clear
-    display_welcome_message
+  public
+
+  def player_moves_loop
+    loop do
+      current_player_moves
+      break if board.someone_won? || board.full?
+      clear_screen_and_display_board if human_turn?
+    end
+  end
+
+  def game_loop
     loop do
       display_board
-      loop do
-        human_moves
-        break if board.someone_won? || board.full?
-        computer_moves
-        break if board.someone_won? || board.full?
-        clear_screen_and_display_board
-      end
+      player_moves_loop
       display_result
       break unless play_again?
       reset
       display_reset_message
     end
+  end
+
+  def play
+    clear
+    display_welcome_message
+    game_loop
     display_goodbye_message
   end
 end
