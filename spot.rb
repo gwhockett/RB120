47d2876166fb16d8ca0f8f18d199112a77bafd56
gwhockett <gwhockett@gmail.com>
2024-02-11 13:23:29 -0500

    test change

--- spot.rb
+++ spot.rb
@@ -541,4 +541,6 @@ home1 = House.new(100_000)
 home2 = House.new(150_000)
 puts "Home 1 is cheaper" if home1 < home2 # => Home 1 is cheaper
 puts "Home 2 is more expensive" if home2 > home1 # => Home 2 is more expensive
+
+test change!!!
 =end
